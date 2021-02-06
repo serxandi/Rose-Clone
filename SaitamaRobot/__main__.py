9e2bb8df241788c8cab6e98a694542a09040cd54
senanoguz <53573382+senanoguz@users.noreply.github.com>
2021-02-07 03:01:30 +0400

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -190,7 +190,7 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton(
-                            text="☑️ Botu qrupunuza əlavə edin",
+                            text=f"☑️ {context.bot.first_name} qrupunuza əlavə edin",
                             url="t.me/{}?startgroup=true".format(
                                 context.bot.username))
                     ],
